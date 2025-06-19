4acce88682199d4546ef1f1028d86f5feb4ac422
github-action-benchmark <github@users.noreply.github.com>
2025-06-19 15:53:09 +0000

    add Benchmark (cargo) benchmark result for 15f83f94b157234edcc2a09e0f455833fd52ba86

---
+++ benchmarks/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1750348389727,
+  "repoUrl": "https://github.com/pedrobergamini/ethrex",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49622509+jrchatruc@users.noreply.github.com",
+            "name": "Javier Rodríguez Chatruc",
+            "username": "jrchatruc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "15f83f94b157234edcc2a09e0f455833fd52ba86",
+          "message": "Merge branch 'main' into main",
+          "timestamp": "2025-06-19T12:05:40-03:00",
+          "tree_id": "c26f10337b6e7fb9cb5ee5cde1bba94d47ba2cb5",
+          "url": "https://github.com/pedrobergamini/ethrex/commit/15f83f94b157234edcc2a09e0f455833fd52ba86"
+        },
+        "date": 1750348388037,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 226006205894,
+            "range": "± 1452487140",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
